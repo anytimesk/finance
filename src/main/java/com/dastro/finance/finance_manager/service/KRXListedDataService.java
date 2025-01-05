09bad07af64cc03e8b0445275ac20a53787381d4
SKY <sk4u.yoon@gmail.com>
2025-01-05 23:57:42 +0900

    엘라스틱서치 krx_list_data index에 데이터 저장하는 기능 추가(Bulk input)

--- src/main/java/com/dastro/finance/finance_manager/service/KRXListedDataService.java
+++ src/main/java/com/dastro/finance/finance_manager/service/KRXListedDataService.java
@@ -3,8 +3,13 @@ package com.dastro.finance.finance_manager.service;
 import java.util.List;
 
 import com.dastro.finance.finance_manager.entity.KRXListedData;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public interface KRXListedDataService {
+
+    public void save(KRXListedData KRXListedData);
+
+    public void saveAll(JsonNode items) throws Exception;
 
     public List<KRXListedData> findAll();
 }
--- src/main/java/com/dastro/finance/finance_manager/service/impl/KRXListedDataServiceImpl.java
+++ src/main/java/com/dastro/finance/finance_manager/service/impl/KRXListedDataServiceImpl.java
@@ -3,6 +3,8 @@ package com.dastro.finance.finance_manager.service.impl;
 import com.dastro.finance.finance_manager.entity.KRXListedData;
 import com.dastro.finance.finance_manager.repo.KRXListedDataRepository;
 import com.dastro.finance.finance_manager.service.KRXListedDataService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +23,30 @@ public class KRXListedDataServiceImpl implements KRXListedDataService {
 
     public KRXListedDataServiceImpl(KRXListedDataRepository KRXListRepo) {
         this.KRXListRepo = KRXListRepo;
+    }
+
+    public void save(KRXListedData KRXListedData) {
+        KRXListRepo.save(KRXListedData);
+    }
+
+    public void saveAll(JsonNode items) throws Exception {
+        List<KRXListedData> listData = StreamSupport.stream(items.spliterator(), false)
+                .map(item -> {
+                    try {
+                        return new ObjectMapper().treeToValue(item, KRXListedData.class);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .peek(data -> log
+                        .debug(() -> String.format("KRXListedData: %s %s", data.getIsinCd(), data.getItmsNm())))
+                .collect(Collectors.toList());
+
+        if (!listData.isEmpty()) {
+            KRXListRepo.saveAll(listData);
+        } else {
+            log.info("No data to save");
+        }
     }
 
     public List<KRXListedData> findAll() {
